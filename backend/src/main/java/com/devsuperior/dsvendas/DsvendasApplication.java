f9f3025e10df057db3919eee5201025e5f899735
nsanchez1310 <nsanchez1310@gmail.com>
2021-05-09 19:23:13 -0300

    Pesquisa por grupo

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.dsvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "com.devsuperior.dsvendas.*" })
 public class DsvendasApplication {
 
 	public static void main(String[] args) {
